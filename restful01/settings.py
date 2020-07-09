b2006b5a73e1fd0094bb985c798e5577e0b4aedc
jessc1 <mmariajessica@gmail.com>
2020-07-09 19:21:58 -0300

    drone app with postgresql

--- restful01/settings.py
+++ restful01/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'restful01.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'drones',
+        'USER': 'postgres',
+        'PASSWORD':'mktj2020',
+        #'PORT': '5432',
     }
 }
 
